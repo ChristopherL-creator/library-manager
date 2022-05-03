731f8feebc7f8fcc19465e26d7583fed09bf379b
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-03 09:26:16 +0200

    3

---
+++ book.js
@@ -0,0 +1,12 @@
+class Book{ 
+
+    constructor(title, author, editor, type, price, copies, pages, yop, discount) 
+
+    bookToString(){ 
+
+    } 
+
+    getPublicPrice(){ 
+// price di partenza + tasse 10% - eventuale discount in % + 30%;
+    }
+}
---
+++ magazine.js
@@ -0,0 +1,12 @@
+class Magazine{ 
+
+    constructor(title, editor, release, cadence, type, price, copies, discount) 
+
+    magazineToString(){ 
+
+    } 
+
+    getPublicPrice(){ 
+// price di partenza + tasse 20% - eventuale discount in % + 30%;
+    }
+}
